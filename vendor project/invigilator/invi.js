6d021cb8be72bb41e01ded103a25b79ddf56749b
Sachin Kumawat <121340987+Sachin958@users.noreply.github.com>
2024-07-27 12:07:40 +0530

    Delete vendor project directory

--- vendor project/invigilator/invi.js
+++
@@ -1,6 +0,0 @@
-const viewreport = document.getElementById('viewreport')
-
-viewreport.addEventListener('click' , function(event){
-    event.preventDefault()
-    window.location.href = '../auditorDetails.html'
-})
